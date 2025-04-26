8c8fb515835f4350fc241d107a5112d2c9266b75
joseph220LQM <133925092+joseph220LQM@users.noreply.github.com>
2025-04-26 17:18:02 -0500

    Delete routes directory

--- routes/userRoutes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {validateCredentials, Signup, getDepartaments} = require('./controllers/users.js');
-
-router.post('/login', validateCredentials);
-router.post('/signup', Signup);
-router.get('/departamentos', getDepartaments);
-
-module.exports = router;
